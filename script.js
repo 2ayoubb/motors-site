5cbf97272ad42b4cf17ac7771b9b7f087243a5b8
2ayoubb <amalmalika088@gmail.com>
2023-08-23 09:58:33 +0100

    site web

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const contactButton = document.getElementById("contactBtn");
+
+    contactButton.addEventListener("click", function() {
+        alert("Feel free to reach out to us using the contact form!");
+    });
+});
